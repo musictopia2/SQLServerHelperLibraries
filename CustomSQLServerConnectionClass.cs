ecea5bedc4446bc143c96a7fa7a37d0fd63b5d94
Andy <musictopia2@hotmail.com>
2021-11-12 11:05:13 -0700

    Add project files.

---
+++ CustomSQLServerConnectionClass.cs
@@ -0,0 +1,20 @@
+﻿namespace SQLServerHelperLibraries;
+public class CustomSQLServerConnectionClass : ISQLServerConnector
+{
+    EnumDatabaseCategory IDbConnector.GetCategory(IDbConnection connection)
+    {
+        if (connection is SqlConnection == false)
+        {
+            throw new CustomBasicException("Should have been a sql server connection.  Rethink");
+        }
+        return EnumDatabaseCategory.SQLServer; //will always be sql server with this class.
+    }
+    IDbConnection IDbConnector.GetConnection(EnumDatabaseCategory category, string connectionString)
+    {
+        if (category != EnumDatabaseCategory.SQLServer)
+        {
+            throw new CustomBasicException("Only sql server is supported for this category.  Rethink");
+        }
+        return new SqlConnection(connectionString);
+    }
+}
